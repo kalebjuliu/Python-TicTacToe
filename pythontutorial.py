ee9c13658cbff6fc562240db69504500739d95e9
Kaleb Juliu <kalebjuliu@gmail.com>
2019-05-20 16:25:21 +0700

    Creating gameboard and error handling

---
+++ pythontutorial.py
@@ -0,0 +1,20 @@
+game = [[0,0,0],
+        [0,0,0],
+        [0,0,0]]
+
+def game_board(game_map, player=0, row=0, column=0, just_display=False):
+    try:
+        print("   0  1  2")
+        if not just_display:
+            game_map[row][column] = player
+        for count, row in enumerate(game_map):
+            print(count, row)
+        return game_map
+
+    except IndexError as e:
+        print("Error: Insert row/column value between 0 and 2", e)
+    except Exception as e:
+        print("Something went wrong")
+
+game = game_board(game, just_display=True)
+game = game_board(game, player=1, row=2, column=1) 
